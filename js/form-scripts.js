b48e428b617313e5e9fc8e1c98719745709b863d
parasolya <ola.shevchuk@gmail.com>
2024-11-03 19:51:27 +0200

    added links to Thanks page

---
+++ js/form-scripts.js
@@ -0,0 +1,5 @@
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+  event.preventDefault(); 
+  window.location.href = "../thanks"; 
+});
+
--- js/modal.js
+++ js/modal.js
@@ -1,17 +1,24 @@
 (() => {
-  const refs = {
-      openModalBtns: document.querySelectorAll("[data-modal-open]"),
-      closeModalBtn: document.querySelector("[data-modal-close]"),
-      modal: document.querySelector("[data-modal]"),
-  };
+  
+    const refs = {
+        openModalBtns: document.querySelectorAll("[data-modal-open]"),
+        closeModalBtn: document.querySelector("[data-modal-close]"),
+        modal: document.querySelector("[data-modal]"),
+    };
 
-  refs.openModalBtns.forEach(btn => {
-      btn.addEventListener("click", toggleModal);
-  });
+    refs.openModalBtns.forEach(btn => {
+        btn.addEventListener("click", toggleModal);
+    });
 
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
 
-  function toggleModal() {
-      refs.modal.classList.toggle("is-hidden");
-  }
+    function toggleModal() {
+        refs.modal.classList.toggle("is-hidden");
+    }
+
+ 
+    document.getElementById("modalForm").addEventListener("submit", function(event) {
+        event.preventDefault(); 
+        window.location.href = "../thanks"; 
+    });
 })();
